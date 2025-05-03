be160b1efb0b952f172053410b6f826bc2c0934e
Mangesh Narwade <mangeshnarwade1999@gmail.com>
2025-05-03 21:01:38 +0530

    first commit and starting's code

---
+++ Ramprasad.java
@@ -0,0 +1,10 @@
+public class Ramprasad {
+public static void main (String[] args) {
+    //  Variables
+   int a = 100;
+   int b = 500;
+   int sum = a +b;
+   System.out.println(sum);
+
+}
+}
---
+++ Ritesh.java
@@ -0,0 +1,24 @@
+
+
+public class Ritesh.Java {
+    
+    import java.util.Scanner;
+
+
+    public static void main (String[] args) {
+    System.out.println("Taking Input From the User");
+    Scanner sc = new.Scanner()
+
+    
+    }}
+    
+
+
+
+
+
+
+
+
+
+
